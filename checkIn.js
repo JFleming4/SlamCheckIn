5872a438be5c05dfe571dfbfea0debf66cfdf763
JFleming4 <justinfleming95@gmail.com>
2016-12-03 12:03:58 -0500

    Implemented the Mongodb hopefully

--- checkIn.js
+++ checkIn.js
@@ -1,8 +1,12 @@
 var http = require("http");
 var dispatcher = require('httpdispatcher');
+var MongoClient = require('mongodb').MongoClient
+
 var PORT = process.env.PORT || 5000;
-var SLACK_VALIDATION_TOKEN = 'vKzzyX96y3iZdTUMJFBeV7sh'
-function handleRequest(request, response){
+var SLACK_VALIDATION_TOKEN = process.env.SLACK_TOKEN;
+var MONGO_URL = process.env.MONGODB_URI;
+
+function handleRequest(request, response) {
     try {
         dispatcher.dispatch(request, response);
     } catch(err) {
@@ -10,37 +14,64 @@ function handleRequest(request, response){
     }
 }
 var server = http.createServer(handleRequest);
-server.listen(PORT, function(){
+server.listen(PORT, function() {
     // console.log('Server listening on: http://localhost:%s', PORT);
 });
 
-dispatcher.onPost('/checkIn', function (req, res)
+function getFormatedDate(timestamp)
 {
+  var date = new Date(timestamp);
+  var day = date.getDate();
+  var month = date.getMonth()+1;
+  var year = date.getFullYear();
+
+  if (day < 10) day = '0' + day;
+  if (month < 10) month = '0' + month;
+  return day + '/' + month+'/' + year;
+}
+dispatcher.onPost('/checkIn', function (req, res) {
   console.log("In Post")
-  if(req.params.token === SLACK_VALIDATION_TOKEN)
-  {
+  if(req.params.token === SLACK_VALIDATION_TOKEN) {
     console.log("Validated")
     day = new Date(req.params.timestamp);
-    if(day.getDay() === 4)
-    {
+    //if(day.getDay() === 4)
+    //{
       checkIn = {
-        "TimeStamp": req.params.timestamp,
+        "Date": getFormatedDate(req.params.timestamp),
         "User_Name": req.params.user_name
       };
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      return res.end(JSON.stringify({"text": "You've been checked in"}));
-    }
-    else
-    {
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      return res.end(JSON.stringify({"text": "Come back thursday at 6:30 in AP 448B"}));
-    }
+      MongoClient.connect(MONGO_URL, function(err, db) {
+        var people = db.collection("people");
+        people.find(checkIn).toArray(function(err, people) {
+          if(people.length > 0) {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            return res.end(JSON.stringify({"text": "You already checked in"}));
+          } else {
+            people.insert(checkIn, function(err, result) {
+              db.close();
+              res.writeHead(200, {'Content-Type': 'application/json'});
+              return res.end(JSON.stringify({"text": "You've been checked in"}));
+            });
+          }
+        });
+      });
+
+    //}
+    //else
+    //{
+      //res.writeHead(200, {'Content-Type': 'application/json'});
+      //return res.end(JSON.stringify({"text": "Come back thursday at 6:30 in AP 448B"}));
+    //}
   }
 })
 
-dispatcher.onGet('/getAtendees', function (req, res)
-{
+dispatcher.onGet('/getAtendees', function (req, res) {
   console.log("In Get")
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  return res.end(JSON.stringify({"text": 'Hello'}));
+  MongoClient.connect(MONGO_URL, function(err, db) {
+    var people = db.collection('people');
+    people.find({}).toArray(function(err, ppl) {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      return res.end(JSON.stringify({"People": ppl}));
+    });
+  });
 })
