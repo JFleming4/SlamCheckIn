44661a931c260edaddf3471ec5ffbe7c179f92e5
JFleming4 <justinfleming95@gmail.com>
2016-12-03 09:56:23 -0500

    Initial Commint

---
+++ checkIn.js
@@ -0,0 +1,39 @@
+var http = require("http");
+var dispatcher = require('httpdispatcher');
+var os = require('os');
+var time = require('time');
+var PORT = 8081;
+var SLACK_VALIDATION_TOKEN = 'vKzzyX96y3iZdTUMJFBeV7sh'
+function handleRequest(request, response){
+    try {
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+var server = http.createServer(handleRequest);
+server.listen(PORT, function(){
+    console.log('Server listening on: http://localhost:%s', PORT);
+});
+
+dispatcher.onPost('/checkIn', function (req, res)
+{
+  if(req.params.token === SLACK_VALIDATION_TOKEN)
+  {
+    day = new Date(req.params.timestamp);
+    if(day.getDay() === 4)
+    {
+      checkIn = {
+        "TimeStamp": req.params.timestamp,
+        "User_Name": req.params.user_name
+      };
+      res.writeHead(200, {'Content-Type': 'application/json'})
+      res.end(JSON.stringify({"text": "You've been checked in"}))
+    }
+    else
+    {
+      res.writeHead(200, {'Content-Type': 'application/json'})
+      res.end(JSON.stringify({"text": "Come back thursday at 6:30 in AP 448B"}))
+    }
+  }
+})
